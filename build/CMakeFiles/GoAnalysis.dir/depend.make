1c0d95843381b1cb395406ef8b2b201db8235cef
mayuke <393810965@qq.com>
2022-02-24 07:30:04 -0800

    [chore] cancel git trace with build directories

--- build/CMakeFiles/GoAnalysis.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for GoAnalysis.
-# This may be replaced when dependencies are built.
--- build/Config.h
+++
@@ -1,5 +0,0 @@
-//the configured options and settings for proj
-#define PROJECT_VERSION_MAJOR  1
-#define PROJECT_VERSION_MINOR  0
-
-/* #undef USE_CRAWL */
